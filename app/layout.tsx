e72bf9a058da194e7923b074d16b08f063f2b734
Noey Ignacio <noeyislearning@gmail.com>
2023-06-04 23:24:48 +0800

    Add `Navbar` to the root `layout.tsx`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,19 @@
+import React from "react";
 
 /** Stylesheets */
 import "@/assets/css/core.css"
 
 /** Fonts */
 import { Rubik } from "next/font/google"
-const fonts = Rubik({ subsets: ['latin'] })
+const globalFont = Rubik({ subsets: ['latin'] })
 
 export const metadata = {
   title: "Zoiquery - SQL Generator",
   description: 'Zoiquery provides a comprehensive toolkit for constructing SQL statements programmatically, eliminating the need for manual query building and enhancing productivity.',
 }
+
+/** Components */
+import Navbar from "@/components/layouts/Navbar/Navbar";
 
 export default function RootLayout({
   children,
@@ -17,8 +21,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={fonts.className}>{children}</body>
+    <html lang="en" className="bg-gray-950 text-white">
+      <body className={globalFont.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
